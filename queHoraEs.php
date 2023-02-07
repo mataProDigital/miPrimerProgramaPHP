9ee114c3f71a325ece65465e2f688c220fbb262e
mataProDigital <ortizinbox1@gmail.com>
2023-02-07 11:02:00 -0800

    Mi primer codigo con PHP

---
+++ queHoraEs.php
@@ -0,0 +1,15 @@
+<?php
+
+$segundos = readline("Ingresa el tiempo en segundos: ");
+
+$horas = (int) ($segundos / 3600);
+$segundos = (int) ($segundos % 3600);
+$minutos = (int) ($segundos / 60);
+$segundos = (int) ($segundos % 60);
+
+echo "Son $horas horas, $minutos minutos, $segundos segundos.";
+
+
+echo "\n";
+
+
